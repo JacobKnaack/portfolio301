0d338c9d803877713ec6b8082851b38f2c00b624
Jacob Knaack <jacobknaack@Jacobs-MacBook-Pro-2.local>
2016-02-03 23:02:43 -0800

    improved organization and added IIFE

--- scripts/events.js
+++ scripts/events.js
@@ -1,26 +1,60 @@
-/* This section handles all events for styling*/
-$(document).ready(function() {
-  $('#info').hide();
-});
 
-$('#about').click( function() {
-  $('#main').hide();
+(function() {
+  /* This section handles all events for styling*/
+  $(document).ready(function() {
+    $('#info').hide();
+    $('#main').hide();
+  });
 
-  $('#intro').hide();
-  $('#intro').fadeIn(300);
+  $('#projects').click( function() {
+    $('#main').fadeIn();
 
-  $('p:first').text('I\'m a student at code fellows learning about website development as well as a technical communicator.');
+    $('#intro').hide();
 
-  $('p:nth-of-type(2)').text('My skills include a proficiency with HTLM, CSS, Javascript as well as expertise in document design and technical writing.');
+    $('footer').hide();
 
-  if ($(window).scrollTop() + $(window).height() > $(document).height() - 50) {
-    $('footer').fadeIn();
-  } else {
-    $('footer').fadeOut();
-  }
-});
+    if ($(window).scrollTop() + $(window).height() > $(document).height() - 50) {
+      $('footer').fadeIn();
+    } else {
+      $('footer').fadeOut();
+    }
+  });
 
-$('#title').click(function() {
-  $('#banner').fadeOut(500);
-  $('#info').fadeIn(500);
-});
+  $('#about').click(function() {
+    $('#main').hide();
+
+    $('#intro').hide();
+    $('#intro').fadeIn(300);
+
+    $('p:first').text('I\'m a student at code fellows learning about website development as well as a technical communicator.');
+    $('p:nth-of-type(2)').text('My skills include a proficiency with HTLM, CSS, Javascript as well as expertise in document design and technical writing.');
+
+    if ($(window).scrollTop() + $(window).height() > $(document).height() - 50) {
+      $('footer').fadeIn();
+    } else {
+      $('footer').fadeOut();
+    }
+  });
+
+  $('#home').click(function () {
+    $('#main').hide();
+
+    $('#intro').hide();
+    $('#intro').fadeIn(300);
+
+    $('p:first').text('Welcome back!');
+    $('p:nth-of-type(2)').text('To learn more about me click the about section above, to look at my work click on projects.');
+
+    if ($(window).scrollTop() + $(window).height() > $(document).height() - 50) {
+      $('footer').fadeIn();
+    } else {
+      $('footer').fadeOut();
+    }
+  });
+
+  $('#title').click(function() {
+    $('#banner').fadeOut(500);
+    $('#info').fadeIn(500);
+    $('footer').fadeIn(500);
+  });
+})(window);
--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,59 +1,44 @@
-function Portfolio(prop) {
-  this.title = prop.title,
-  this.pubDate = prop.pubDate,
-  this.url = prop.url,
-  this.img = prop.img,
-  this.body = prop.body;
-};
+(function(module) {
+  function Portfolio(prop) {
+    this.title = prop.title,
+    this.pubDate = prop.pubDate,
+    this.url = prop.url,
+    this.img = prop.img,
+    this.body = prop.body;
+  };
 
-Portfolio.prototype.toHtml = function () {
-  var compileProject = Handlebars.compile($('#project-template').html());
-  var html = compileProject(this);
+  Portfolio.prototype.toHtml = function () {
+    var compileProject = Handlebars.compile($('#project-template').html());
+    var html = compileProject(this);
 
-  return html;
-};
+    return html;
+  };
 
-//working on json ajax request functions
-Portfolio.all = [];
+  //working on json ajax request functions
+  Portfolio.all = [];
 
-Portfolio.load = function(projects) {
-  projects.sort(function(p1, p2) {
-    return(new Date(p2.pubDate)) - (new Date(p1.pubDate));
-  });
-
-  projects.forEach(function(sample) {
-    Portfolio.all.push(new Portfolio(sample));
-  });
-};
-
-Portfolio.fetch = function() {
-  if (localStorage.projects) {
-    Portfolio.load(JSON.parse(localStorage.projects));
-    renderProjects.show();//function to render the page
-  } else {
-    $.getJSON('/data/projects.json', function(projects) {
-      Portfolio.load(projects);
-      localStorage.projects = JSON.stringify(projects);
-      renderProjects.show();//call function again to render the page
+  Portfolio.load = function(projects) {
+    projects.sort(function(p1, p2) {
+      return(new Date(p1.pubDate)) - (new Date(p2.pubDate));
     });
-  }
-};
-//end of ajax programming section
 
-$('#projects').click( function() {
-  $('#main').fadeIn();
+    projects.forEach(function(sample) {
+      Portfolio.all.push(new Portfolio(sample));
+    });
+  };
 
-  $('#intro').hide();
+  Portfolio.fetch = function() {
+    if (localStorage.projects) {
+      Portfolio.load(JSON.parse(localStorage.projects));
+      renderProjects.show();//function to render the page
+    } else {
+      $.getJSON('/data/projects.json', function(projects) {
+        Portfolio.load(projects);
+        localStorage.projects = JSON.stringify(projects);
+        renderProjects.show();//call function again to render the page
+      });
+    }
+  };
 
-  $('footer').hide();
-});
-
-$(window).scroll(function () {
-  if ($(window).scrollTop() + $(window).height() > $(document).height() - 50) {
-    $('footer').fadeIn();
-  } else {
-    $('footer').fadeOut();
-  }
-});
-
-$('#main').hide();
+  module.Portfolio = Portfolio;
+})(window);
